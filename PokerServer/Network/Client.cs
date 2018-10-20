d91b3eeedd7299b0fdacec3fd309defe7d260680
Mike Maris <mmaris@verif-y.com>
2018-10-20 07:19:37 -0400

    initial server code

---
+++ PokerServer/Network/Client.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PokerServer.Network
+{
+    class Client
+    {
+        public string ID { get; private set; }
+        public IPEndPoint EndPoint { get; private set; }
+
+        Socket s;
+        public Client(Socket accepted)
+        {
+            s = accepted;
+            ID = Guid.NewGuid().ToString();
+            EndPoint = (IPEndPoint)s.RemoteEndPoint;
+            s.BeginReceive(new byte[] { 0 }, 0, 0, 0, callback, null);
+        }
+
+        void callback(IAsyncResult ar)
+        {
+            try
+            {
+                s.EndReceive(ar);
+
+                byte[] buf = new byte[8192];
+
+                int rec = s.Receive(buf, buf.Length, 0);
+
+                if (rec < buf.Length)
+                {
+                    Array.Resize<byte>(ref buf, rec);
+                }
+
+                if (Received != null)
+                {
+                    Received(this, buf);
+                }
+
+                s.BeginReceive(new byte[] { 0 }, 0, 0, 0, callback, null);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Close();
+                if(Disconnected != null)
+                {
+                    Disconnected(this);
+                }
+            }
+            
+        }
+
+        public void Close()
+        {
+            s.Close();
+            s.Dispose();
+        }
+
+        public delegate void ClientReceivedHandler(Client sender, byte[] data);
+        public delegate void ClientDisconnectedHandler(Client sender);
+
+        public event ClientReceivedHandler Received;
+        public event ClientDisconnectedHandler Disconnected;
+    }
+}
---
+++ PokerServer/Network/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+
+namespace PokerServer.Network
+{
+    class Server
+    {
+        Listener listener;
+        public List<Client> Clients { get; private set; }
+        public Server()
+        {
+            Clients = new List<Client>();
+            listener = new Listener(8);
+            listener.SocketAccepted += new Listener.SocketAcceptedHandler(listener_SocketAccepted);
+            
+        }
+
+        void listener_SocketAccepted(Socket e)
+        {
+            Client client = new Client(e);
+            client.Received += new Client.ClientReceivedHandler(cleint_Received);
+            client.Disconnected += new Client.ClientDisconnectedHandler(client_Disconnected);
+            
+        }
+
+        void client_Disconnected(Client sender)
+        {
+            Clients.Remove(sender);
+        }
+
+        void cleint_Received(Client sender, byte[] data)
+        {
+            Clients.Add(sender);
+        }
+    }
+}
